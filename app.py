30ce0f4d686c925f158fe6e859d4480e4262dd9d
MafireBema <Bema@mafire.de>
2025-07-27 21:21:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
-import sqlite3
-def init_db():
-    ...
-init_db()
+from flask import Flask, request
+from webhook import stripe_webhook
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Stripe Telegram Access läuft!"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    return stripe_webhook(request)
+
+if __name__ == "__main__":
+    app.run(debug=True)
