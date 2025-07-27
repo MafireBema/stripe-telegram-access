938608ac71e068e70f321fde27142a5a3c1ebf93
MafireBema <Bema@mafire.de>
2025-07-27 21:21:56 +0200

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,6 +1,16 @@
 import sqlite3
+
 conn = sqlite3.connect("users.db")
 c = conn.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY)")
+
+c.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        email TEXT NOT NULL UNIQUE
+    )
+''')
+
 conn.commit()
 conn.close()
+
+print("✅ Datenbank erstellt: users.db")
