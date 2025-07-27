939e09fa7075d12eb1c674bcb214df95a1ad646e
MafireBema <Bema@mafire.de>
2025-07-27 21:07:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,17 @@ from webhook import app
 
 if __name__ == "__main__":
     app.run()
+import sqlite3
+
+def init_db():
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            user_id TEXT PRIMARY KEY
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+init_db()
